b18394f65b2472bcd86fd428e11883ea124337f4
flow.gunso <flow.gunso@gmail.com>
2018-01-27 13:06:51 +0100

    now support custom timed loops (with a 30s default)

--- batch/Always kill Paint/README.md
+++ batch/Always kill Paint/README.md
@@ -1,4 +1,5 @@
 # Always kill Paint
 
-Endlessly kill all existing instances of mspaint.exe every 30s.
+Endlessly kill all existing instances of mspaint.exe every _n_ seconds. Not specifying _n_ defaults to 30s loops.
+
 Used along a Brother document scanner and the ControlCenter4 software which open one instance of Paint per image scan.
--- batch/Always kill Paint/always_kill_paint.bat
+++ batch/Always kill Paint/always_kill_paint.bat
@@ -1,4 +1,32 @@
+@ECHO OFF
+IF NOT "%~2"=="" GOTO parse_err
+IF NOT "%~1"=="" GOTO check_is_numeric
+IF "%~1"=="" (
+	echo No argument given, default to 30s loop.
+	SET /a a=30 >nul
+	GOTO loop
+)
+
+:check_is_numeric
+SET /a a=%~1+0 >nul
+IF %a% == 0 (
+	IF NOT "%a%"=="0" ( GOTO parse_err
+  ) ELSE ( GOTO check_is_gt_than_zero )
+) ELSE (GOTO check_is_gt_than_zero )
+
+:check_is_gt_than_zero
+IF %a% GTR 0 ( GOTO loop
+) ELSE ( GOTO gt_zero_err )
+
+:parse_err
+ECHO This script can read only one argument: the number of seconds per loops.
+EXIT /B
+
+:gt_zero_err
+ECHO The number of seconds per loop must be greater than 0.
+EXIT /B
+
 :loop
-timeout 30
-taskkill /im mspaint.exe
-goto loop
+TIMEOUT %a%
+TASKKILL /im mspaint.exe
+GOTO loop
